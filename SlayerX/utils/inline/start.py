51b75995e234dd938350684f064e351551731d0a
Hector532 <86250772+Hector532@users.noreply.github.com>
2024-04-11 14:20:58 +0200

    Update start.py

--- SlayerX/utils/inline/start.py
+++ SlayerX/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_[Source Dev], url="https://t.me/G0_LX"),
         ],
     ]
     return buttons
